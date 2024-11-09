fa121f98eb391f07c183a32f03f732801241ae2c
Spoopsmcgee12-s <156264077+Spoopsmcgee12-s@users.noreply.github.com>
2024-11-09 00:16:44 -0500

    code of game

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -2,6 +2,10 @@ extends CanvasLayer
 
 @onready var player : CharacterBody2D = get_tree().get_first_node_in_group("Player")
 @onready var Generation : Node = $"../Generation"
+@onready var grid : PackedScene = load("res://Eroz-s-Adventure/Scenes/mini_map_grid.tscn")
+
+func _ready() -> void:
+	generate_mini_map()
 
 func _process(delta) -> void:
 	$"Stat Block/Coin Label".text = str(Global.coin)
@@ -58,3 +62,19 @@ func _process(delta) -> void:
 			$"Health Bar/Player Health2".frame = 0
 			$"Health Bar/Player Health3".frame = 0
 			$"Health Bar/Player Health4".frame = 0
+
+
+func generate_mini_map() -> void:
+	$"Mini Map/GridContainer".columns = Generation.map_width
+	for i in range(Generation.map_height):
+		for j in range(Generation.map_width):
+			var panel = grid.instantiate()
+			if Generation.map[j][i] == false:
+				panel.modulate = "ffff00"
+			else:
+			#Error:Invalid assignment of property or key 'is_in_room' 
+			#with value of type 'bool' on a base object of type 
+			#'Panel (mini_map_grid.gd)'.
+				panel.is_in_room = true 
+			panel.pos = Vector2i(j, i)
+			$"Mini Map/GridContainer".add_child(panel)
