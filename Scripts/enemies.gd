ff615240154b35424bf99e3cbefe512037c2469a
Spoopsmcgee12-s <156264077+Spoopsmcgee12-s@users.noreply.github.com>
2024-11-09 13:16:30 -0500

    Update enemies.gd

--- Scripts/enemies.gd
+++ Scripts/enemies.gd
@@ -7,12 +7,12 @@ var player : CharacterBody2D
 func _ready() -> void:
 	player = get_tree().get_nodes_in_group("Player")[0]
 	check_enemies()  
-	player.connect("player_moved", Callable(self, "on_player_moved"))
+	player.connect("player_moved", Callable(self, "_on_player_moved"))
 
 func check_enemies() -> void:
 	enemies = get_tree().get_nodes_in_group("Enemy")
 
-func on_player_moved() -> void:
+func _on_player_moved() -> void:
 	for enemy in enemies:
 		if enemy:
 			enemy.move()
